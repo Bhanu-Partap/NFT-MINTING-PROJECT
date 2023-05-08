8072522ff76d5313170dea1052a06175d27eff2a
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-05-08 12:49:45 +0530

    NFT MINTING

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Row, Col} from "react-bootstrap"
+
+export default function Homepage() {
+  return (
+    <div className="container">
+      <Row>
+        <Col>
+            <h4>OUR MINT IS LIVE</h4>
+            <h1>BORED APE</h1>
+            <h1>NFT COLLECTION</h1>
+            <p>8,888 Unique Undercover Creatures and Cars, Start Minting</p>
+            <p>The 8th of MAY 2023</p>
+        </Col>
+        <Col></Col>
+      </Row>
+    </div>
+  )
+}
