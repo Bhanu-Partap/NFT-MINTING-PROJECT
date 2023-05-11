77b371dafa6bfa614b79dc37bf59e89292ad730e
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-05-11 09:44:39 +0530

    Mint NFTs

---
+++ src/Pages/Collection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Collection() {
+  return (
+    <>
+        <section>
+            <div className="container">
+                <Row>
+                    
+                </Row>
+            </div>
+
+        </section>
+    </>
+  )
+}
